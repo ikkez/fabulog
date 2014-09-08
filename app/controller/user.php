d930103473a5bd0d3b385c93a0f3d41ede373870
ikkez <ikkez0n3@gmail.com>
2014-09-08 16:22:54 +0200

    prohibit changes on demo-admin

--- app/controller/user.php
+++ app/controller/user.php
@@ -24,5 +24,32 @@ class User extends Resource {
 			'content' => $this->resource->find(),
 		);
 	}
+	public function post($f3, $params) {
+		$msg = \FlashMessage::instance();
+		if (isset($params['id'])) {
+			// update existing
+			$this->resource->load(array('_id = ?', $params['id']));
+			if (!$this->resource->dry() && $this->resource->username == 'admin') {
+				$msg->addMessage("You are not allowed to change the demo-admin",'danger');
+				$f3->reroute('/admin/'.$params['module']);
+				return;
+			}
+		}
+		parent::post($f3,$params);
+	}
+
+	public function delete($f3, $params) {
+		$this->resource->reset();
+		$msg = \FlashMessage::instance();
+		if (isset($params['id'])) {
+			$this->resource->load(array('_id = ?', $params['id']));
+			if (!$this->resource->dry() && $this->resource->username == 'admin') {
+				$msg->addMessage("You are not allowed to delete the demo-admin",'danger');
+				$f3->reroute('/admin/'.$params['module']);
+				return;
+			}
+		}
+		$f3->reroute($f3->get('SESSION.LastPageURL'));
+	}
 
 }
